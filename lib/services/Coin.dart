505a2c753c13b7fa8313fd5a7002486dabeb0527
ffthkrdg <ffatihkaradag@gmail.com>
2021-01-10 07:57:02 +0300

    initial commit

---
+++ lib/services/Coin.dart
@@ -0,0 +1,61 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+const List<String> currenciesList = [
+  'AUD',
+  'BRL',
+  'CAD',
+  'CNY',
+  'EUR',
+  'GBP',
+  'HKD',
+  'IDR',
+  'ILS',
+  'INR',
+  'JPY',
+  'MXN',
+  'NOK',
+  'NZD',
+  'PLN',
+  'RON',
+  'RUB',
+  'SEK',
+  'SGD',
+  'USD',
+  'TRY'
+];
+
+const List<String> cryptoList = [
+  'BTC',
+  'ETH',
+  'LTC',
+];
+
+//curl https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey=D8879841-F1D8-4FD5-BC67-003D1AA721BB
+const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate';
+const apiKey = 'DBC951C8-991F-49EC-95BB-8AFD7353D5B2';
+const bitcoinAverageURL = 'https://rest.coinapi.io/v1/exchangerate';
+
+class Coin {
+  Future getCoinData(String selectedCurrency) async {
+    Map<String, String> cryptoPrices = {};
+
+    for (String crypto in cryptoList) {
+      String requestURL =
+          '$coinAPIURL/$crypto/$selectedCurrency?apikey=$apiKey';
+      http.Response response = await http.get(requestURL);
+
+      if (response.statusCode == 200) {
+        var decodedData = jsonDecode(response.body);
+        double lastPrice = decodedData['rate'];
+        cryptoPrices[crypto] = lastPrice.toStringAsFixed(0);
+      } else {
+        print(response.statusCode);
+        throw 'Problem with the get request';
+      }
+    }
+
+    return cryptoPrices;
+  }
+}
--- lib/ui/HomeScreen.dart
+++ lib/ui/HomeScreen.dart
@@ -1,3 +1,8 @@
+import 'dart:io';
+
+import 'package:bitcoin_tracking/services/Coin.dart';
+import 'package:bitcoin_tracking/widgets/CryptoCard.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -6,23 +11,111 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  String selectedCurrency = 'TRY';
+
+  DropdownButton<String> androidDropdown() {
+    List<DropdownMenuItem<String>> dropdownItems = [];
+    for (String currency in currenciesList) {
+      var newItem = DropdownMenuItem(
+        child: Text(currency),
+        value: currency,
+      );
+      dropdownItems.add(newItem);
+    }
+
+    return DropdownButton<String>(
+      value: selectedCurrency,
+      items: dropdownItems,
+      onChanged: (value) {
+        setState(() {
+          selectedCurrency = value;
+          getData();
+        });
+      },
+    );
+  }
+
+  CupertinoPicker iOSPicker() {
+    List<Text> pickerItems = [];
+    for (String currency in currenciesList) {
+      pickerItems.add(Text(currency));
+    }
+
+    return CupertinoPicker(
+      backgroundColor: Colors.lightBlue,
+      itemExtent: 32.0,
+      onSelectedItemChanged: (selectedIndex) {
+        print(selectedIndex);
+        setState(() {
+          selectedCurrency = currenciesList[selectedIndex];
+        });
+      },
+      children: pickerItems,
+    );
+  }
+
+  Map<String, String> coinValues = {};
+
+  bool isWaiting = false;
+
+  void getData() async {
+    isWaiting = true;
+    try {
+      var data = await Coin().getCoinData(selectedCurrency);
+      isWaiting = false;
+      setState(() {
+        coinValues = data;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Bitcoin Tracker"),
+        actions: [Platform.isIOS ? iOSPicker() : androidDropdown()],
         centerTitle: true,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Bitcoin Tracker Real Time'),
-          ],
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              CryptoCard(
+                cryptoCurrency: 'BTC',
+                selectedCurrency: selectedCurrency,
+                value: isWaiting ? '?' : coinValues['BTC'],
+              ),
+              CryptoCard(
+                cryptoCurrency: 'ETH',
+                selectedCurrency: selectedCurrency,
+                value: isWaiting ? '?' : coinValues['ETH'],
+              ),
+              CryptoCard(
+                cryptoCurrency: 'LTC',
+                selectedCurrency: selectedCurrency,
+                value: isWaiting ? '?' : coinValues['LTC'],
+              ),
+            ],
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          getData();
+        },
         tooltip: 'Increment',
         child: Icon(Icons.refresh),
       ),
